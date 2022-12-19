e0848901812b508fa38d41969b597d9fe456bd98
Fadi Atamny <fadi@dataloop.ai>
2022-12-19 14:07:36 +0200

    renamed cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for file in SVGS/*; do mv "$file" `echo $file | tr ' ' '-'` ; done
-for file in SVGS/*.svg ; do mv $file ${file//icon-dl-/} ; done
-for file in SVGS/*.svg ; do mv $file ${file//icon-di-/} ; done
+for file in docs/assets/*; do mv "$file" `echo $file | tr ' ' '-'` ; done
+for file in docs/assets/*.svg ; do mv $file ${file//icon-dl-/} ; done
+for file in docs/assets/*.svg ; do mv $file ${file//icon-di-/} ; done
 
 echo 'Icon names cleaned'
