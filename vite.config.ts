b7a3e88d04f35a89bc8b9777215a8e40607520ba
Fadi Atamny <fadi@dataloop.ai>
2023-03-21 17:19:01 +0200

    added new deploy docs action

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'docs'
-  },
   base: '/icons/',
   optimizeDeps: {
     include: ['lodash', '@dataloop-ai/components']
