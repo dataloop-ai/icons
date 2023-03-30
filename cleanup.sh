94fec26e421502250317cf2e259bc004d8eff43a
Rotem Shaham <rotemshaham@dataloop.ai>
2023-03-30 16:07:49 +0300

    NEW ICONS - 03-30-23 04:07:49 PM

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for file in docs/assets/*; do mv "$file" `echo $file | tr ' ' '-'` ; done
-for file in docs/assets/*.svg ; do mv $file ${file//icon-dl-/} ; done
-for file in docs/assets/*.svg ; do mv $file ${file//icon-di-/} ; done
+for file in assets/*; do mv "$file" `echo $file | tr ' ' '-'` ; done
+for file in assets/*.svg ; do mv $file ${file//icon-dl-/} ; done
+for file in assets/*.svg ; do mv $file ${file//icon-di-/} ; done
 
 echo 'Icon names cleaned'
