f1315a18eff8997c3eea0298f18b6223646be431
Fadi Atamny <fadi@dataloop.ai>
2024-01-16 18:55:27 +0200

    NEW ICONS - 01-16-24 06:55:27 PM

---
+++ types.d.ts
@@ -0,0 +1,18 @@
+export declare enum COLORED_ICONS {
+    DISCOVER = "discover",
+    DINER = "diner",
+    CONFETTI = "confetti",
+    VISA = "visa",
+    MASTERCARD = "mastercard",
+    DEBIT = "debit",
+    AMERICAN_EXPRESS = "american_express",
+    JCB = "jcb",
+    LAUNCH = "launch",
+    UNIONPAY = "unionpay",
+    GOOGLE = "google",
+    AWS_LIGHT = "aws-light",
+    AWS_DARK = "aws-dark",
+    HUGGING_FACE = "hugging-face",
+    NVIDIA = "nvidia",
+    OPEN_MM_LABS = "open-mm-labs"
+}
---
+++ types.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+exports.COLORED_ICONS = void 0;
+var COLORED_ICONS;
+(function (COLORED_ICONS) {
+    COLORED_ICONS["DISCOVER"] = "discover";
+    COLORED_ICONS["DINER"] = "diner";
+    COLORED_ICONS["CONFETTI"] = "confetti";
+    COLORED_ICONS["VISA"] = "visa";
+    COLORED_ICONS["MASTERCARD"] = "mastercard";
+    COLORED_ICONS["DEBIT"] = "debit";
+    COLORED_ICONS["AMERICAN_EXPRESS"] = "american_express";
+    COLORED_ICONS["JCB"] = "jcb";
+    COLORED_ICONS["LAUNCH"] = "launch";
+    COLORED_ICONS["UNIONPAY"] = "unionpay";
+    COLORED_ICONS["GOOGLE"] = "google";
+    COLORED_ICONS["AWS_LIGHT"] = "aws-light";
+    COLORED_ICONS["AWS_DARK"] = "aws-dark";
+    COLORED_ICONS["HUGGING_FACE"] = "hugging-face";
+    COLORED_ICONS["NVIDIA"] = "nvidia";
+    COLORED_ICONS["OPEN_MM_LABS"] = "open-mm-labs";
+})(COLORED_ICONS = exports.COLORED_ICONS || (exports.COLORED_ICONS = {}));
