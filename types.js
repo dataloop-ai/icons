537f10627e380cb3aee9b2d22ca797baf2328b39
Fadi Atamny <fadi@dataloop.ai>
2024-02-19 14:35:02 +0200

    NEW ICONS - 02-19-24 02:35:02 PM

--- types.js
+++ types.js
@@ -17,6 +17,7 @@ export var COLORED_ICONS;
     COLORED_ICONS["NVIDIA"] = "nvidia";
     COLORED_ICONS["OPEN_MM_LABS"] = "open-mm-labs";
     COLORED_ICONS["DATALOOP"] = "dataloop";
+    COLORED_ICONS["DATALOOP_NEW"] = "dataloop-new";
     COLORED_ICONS["DATALOOP_OLD"] = "dataloop-old";
     COLORED_ICONS["AMD"] = "amd";
     COLORED_ICONS["INTEL"] = "intel";
