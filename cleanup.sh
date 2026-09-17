87c5a3383136b953d0bc1ff7d9a851ca4722db3d
Guy Twito <guy.twito@dellteam.com>
2026-09-17 14:14:39 +0300

    fit cleanup scipt to windows

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for file in assets/*; do mv "$file" `echo $file | tr ' ' '-'` ; done
-for file in assets/*.svg ; do mv $file ${file//icon-dl-/} ; done
-for file in assets/*.svg ; do mv $file ${file//icon-di-/} ; done
+for file in assets/*; do new=`echo $file | tr ' ' '-'` ; [ "$file" != "$new" ] && mv "$file" "$new" ; done
+for file in assets/*.svg ; do new=${file//icon-dl-/} ; [ "$file" != "$new" ] && mv $file $new ; done
+for file in assets/*.svg ; do new=${file//icon-di-/} ; [ "$file" != "$new" ] && mv $file $new ; done
 
 echo 'Icon names cleaned'
